44fd99886d7cdb2c64e42f9b27fff708338da41e
Mahesh Bompilwar <mahesh.bompilwar2@gmail.com>
2020-09-16 15:17:17 +0530

    added dots in java programming

--- Customer.java
+++ Customer.java
@@ -1,6 +1,6 @@
 public class Customer{
     Customer(){
         System.out.println("Hello");
-        System.out.println("Java Programming");
+        System.out.println("Java Programming....");
     }
 }
