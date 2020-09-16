b6f9656fe3ef3a2864b0c510a09a8383c1ec3b37
Mahesh Bompilwar <mahesh.bompilwar2@gmail.com>
2020-09-16 16:37:39 +0530

    Student class added

---
+++ Student.java
@@ -0,0 +1,12 @@
+public class Student{
+
+
+    private String name;
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.name="Sachin";
+        System.out.println("Student name: "+student.name);
+    }
+
+}   
