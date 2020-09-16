047e813d8949e8a214cb4acb19a556c074da2bd6
Mahesh Bompilwar <mahesh.bompilwar2@gmail.com>
2020-09-16 16:44:29 +0530

    Parameterized contructor added in Student class

--- Student.java
+++ Student.java
@@ -2,11 +2,18 @@ public class Student{
 
 
     private String name;
+    private int rollNo;
+
+    public Student(String name, int rollNo){
+
+        this.name=name;
+        this.rollNo=rollNo;
+    }
 
     public static void main(String[] args) {
-        Student student = new Student();
-        student.name="Sachin";
+        Student student = new Student("Sachin", 123);
         System.out.println("Student name: "+student.name);
+        System.out.println("Student Roll No : "+student.rollNo);
     }
 
 }   
