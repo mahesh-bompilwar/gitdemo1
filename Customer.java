c198771f98cdbc8e42ccc86f3fad06ed82e9a83d
Mahesh Bompilwar <mahesh.bompilwar2@gmail.com>
2020-09-16 15:24:08 +0530

    Main method added

--- Customer.java
+++ Customer.java
@@ -3,4 +3,7 @@ public class Customer{
         System.out.println("Hello");
         System.out.println("Java Programming....");
     }
+    public static void main(String[] args) {
+        Customer customer= new Customer();
+    }
 }
